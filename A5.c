96dd9d5dbb7005f44ba913b35431f538ad76214a
Robin65 <robinheyer65@gmail.com>
2022-09-14 17:56:14 +0200

    Add files via upload

---
+++ A5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int stringtoint (char *arr ) {
+
+int i;
+int value;
+int number;
+int trig;
+
+int main()
+{
+char arr[10]
+printf("eingabe");
+scanf("%s" ,arr);
+int a = stingtoint (arr);
+printf ("\n %d",ans);
+return 0;
+
+}
