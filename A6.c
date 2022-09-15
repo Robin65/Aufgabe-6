b2316aba7f1c308aba20fa239e6629baf8eef3c4
Robin65 <robinheyer65@gmail.com>
2022-09-15 10:39:46 +0200

    Update A6.c
    Modify the code,does not yet do what it should

--- A6.c
+++ A6.c
@@ -2,7 +2,9 @@
 #include<math.h>
 
 #include<string.h>
-int stringtoint (char *arr ) {
+
+
+int stringtoint (char *arr ) { //definierung der funktion 
 
 int i;
 int value;
@@ -10,21 +12,35 @@ int number;
 int trig;
 
   
-  trig = 1;
-  i = value = 0;
-  for (i=0; i<strlen(arr);i++)
+  trig = 1; //set trig true 
+  i = value = 0; //var value && i is 0;
+  for 
+    (i=0; i<strlen(arr);i++) // for-loop from 0 to string lenght
   {
-      
-  
-  
-  
+    if  
+      (i==0 && arr[i]=='-') // if arr eine negative Zahl enthält
+    //then
+    {
+      trig = -1;
+    continue;
+    }
+    
+    number = arr[i] - '0'; 
+    
+    value = value * 10 + number;
+    }
+     value = value * trig; 
+    return value;  
+    }
+   //end_if;
 int main()
+//then
 {
 char arr[10]
-printf("eingabe");
-scanf("%s" ,arr);
-int a = stingtoint (arr);
-printf ("\n %d",ans);
-return 0;
-
+printf("eingabe"); //aufforderung zur eingabe eines Strings als zahl.
+scanf("%s" ,arr); //einlesen der iengabe in die var arr
+int result = stingtoint (arr); //ausführeung der funktion und übergabe in die Variabele result die vorher definiert war. 
+printf ("\n %d",ans); //rückgabe der zahl
+return 0; 
 }
+//end_if;
